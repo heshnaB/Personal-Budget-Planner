eada078354b84519eb6657fc1f1dc3a60860fa53
heshnaB <heshna599@gmail.com>
2025-08-23 22:21:37 -0600

    Added the html layout to showcase the chages made for adding a transaction and displaying the transaction.

--- templates/script.js
+++ templates/script.js
@@ -1,12 +1,11 @@
 //  HTML form submission handling.
-
 // 1. Create the transaction array to store all the transaction objects.
-let transactions = {};
+let transactions = [];
 
 //2. Grab the form and inputs
 const form = document.getElementById("transaction-form");
 const typeInput = document.getElementById("type");
-const amountInput = document.getElementById.ById("amount");
+const amountInput = document.getElementById("amount");
 const categoryInput = document.getElementById("category");
 const dateInput = document.getElementById("date");
 const notesInput = document.getElementById("notes");
@@ -23,10 +22,28 @@ form.addEventListener("submit", function(event) {
         category: categoryInput.value,
         date: dateInput.value,
         notes: notesInput.value
-    }
-})
+    };
 
-//4. Add to array and clear form
-transactions.push(transaction);
-form.requestFullscreen();
-console.log(transactions);
+    //4. Add to array and clear form
+    transactions.push(transaction);
+    //form.requestFullscreen();
+    console.log(transactions);
+
+    //5. Render updated list
+    renderTransactions();
+});
+
+
+
+//Display the transactions
+function renderTransactions() {
+    // Clear the old list
+    transactionList.innerHTML = "";
+
+    
+    transactions.forEach(tx => {
+        const item = document.createElement("div");
+        item.textContent = `${tx.date} | ${tx.type.toUpperCase()} | $${tx.amount} | ${tx.category} | ${tx.notes}`;
+        transactionList.appendChild(item);
+    });
+};
